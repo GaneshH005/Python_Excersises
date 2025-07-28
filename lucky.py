285dea1665e6497ec81ebc5581328017e41ddecf
GaneshH005 <gmhorakeri@gmail.com>
2025-07-28 11:40:58 +0530

    using else instead of elif

--- lucky.py
+++ lucky.py
@@ -1,11 +1,15 @@
-lucky = 9
-guess = 20
+Name of student = input("enter the student name")
+eng = 90
+sci = 98
+math = 88
+total = ((eng+sci+math)/3)
 
-if guess < lucky - 2 :
-    print("Too low")
-elif guess > lucky + 2 :
-    print("Too high")
-elif guess == lucky :
-    print("correct")
-elif (guess >= lucky - 2) and (guess <= lucky + 2):
-    print ("you are Almost There")
+if total > 90:
+    print("Grade A")
+elif total > 80 and total < 90:
+    print("Grade B")
+elif total < 35:
+    print("fail")
+else:
+    print("Avrage grade")
+    
