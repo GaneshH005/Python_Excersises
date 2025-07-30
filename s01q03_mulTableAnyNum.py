84e792fc246df81296347d724f49126d3c3a82dd
GaneshH005 <gmhorakeri@gmail.com>
2025-07-30 15:48:42 +0530

    Add files via upload

---
+++ s01q03_mulTableAnyNum.py
@@ -0,0 +1,8 @@
+# num = 17
+num = int(input("enter the multiplcation table of : "))
+
+for i in range(1,11):
+    mul_res = num * i
+    # print(mul_res)
+    print(num," * ",i , "=", mul_res)
+    i = i+1
